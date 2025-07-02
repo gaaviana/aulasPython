11b4157a3f2a200b6f9a9161f413812d989aeeb9
Gabriel Viana <gaa.vianaa@gmail.com>
2025-07-02 20:23:08 -0300

    loops de repetição

---
+++ loops.py
@@ -0,0 +1,25 @@
+print('Exemplo - 1 (for)')
+for n in range(10):
+    print(n)
+
+print('---------------------')
+print('Exemplo - 2 (for)')
+"""podemos mudar o valor inicial 
+do laço de repetição"""
+
+for x in range(5, 16):
+    print(x)
+
+print('---------------------')
+print('Exemplo - 3 (for)')
+"""podemos aplicar em ordem decrescente"""
+for y in range(10, 0, -1):
+    print(y)
+
+print('---------------------')
+print('---------------------')
+print('Exemplo - 1 (while)')
+z = 1;
+while z<=15:
+    print(z);
+    z = z+1
